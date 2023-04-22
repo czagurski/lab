a7b5fad42f6317fa2cfbc49fbf53444c226cf4b9
czagurski <131494048+czagurski@users.noreply.github.com>
2023-04-22 01:05:41 -0500

    Precaustion save

--- test_account.py
+++ test_account.py
@@ -1,4 +1,4 @@
-from pytest import*
+import pytest
 from account import*
 
 
@@ -10,10 +10,20 @@ class Test:
         del self.a1
 
     def test_init(self):
-        pass
+        assert self.a1.get_name() == 'Jane'
+        assert self.a1.get_age() == 10
+        assert self.a1.get_name() == 'John'
+        assert self.a1.get_age() == 20
 
     def test_deposit(self):
-        pass
+        assert self.a1.deposit(-1.5) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(1.5) is True
+        assert self.a1.get_balance() == pytest.approx(1.5, abs=0.01)
 
     def test_withdraw(self):
         pass
