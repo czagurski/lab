ebea49584380be5cfaaf025f548fb25f2cebfa77
czagurski <131494048+czagurski@users.noreply.github.com>
2023-04-21 20:12:53 -0500

    Added second(test) file

--- account.py
+++ account.py
@@ -1,5 +1,5 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name:str):
         pass
 
     def deposit(self, amount):
---
+++ test_account.py
@@ -0,0 +1,19 @@
+from pytest import*
+from account import*
+
+
+class Test:
+    def setup_method(self):
+        self.a1 = Account('John')
+
+    def teardown_method(self):
+        del self.a1
+
+    def test_init(self):
+        pass
+
+    def test_deposit(self):
+        pass
+
+    def test_withdraw(self):
+        pass
